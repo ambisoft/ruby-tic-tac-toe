4a4dc01249714aa7146d0fb600f8474301273f44
ambisoft <mark@ambisoft.pl>
2014-01-04 16:36:12 +0100

    initial import (contains bugs)

---
+++ src/tic_tac_toe.rb
@@ -0,0 +1,50 @@
+class TicTacToe
+  
+  attr_reader :current_player
+  
+  def initialize
+    
+    @current_player = 'O'    
+    
+    @plays = { 'X' => [], 'O' => [] }
+    
+    @horizontal_winner = [ [1, -1], [1, 0], [1, 1] ] ||
+                         [ [0, -1], [0, 0], [0, 1] ] ||
+                         [ [-1, -1], [-1, 0], [-1, 1] ]
+                       
+    @vertical_winner = [ [1, -1], [0, -1], [-1, -1] ] ||
+                       [ [1, 0], [0, 0], [0, -1] ] ||
+                       [ [1, 1], [0, 1], [-1, 1] ]
+                     
+    @diagonal_winner = [ [1, -1], [0, 0], [-1, 1] ] ||
+                       [ [-1, -1], [0, 0], [1, 1] ]
+    
+  end
+  
+  def start_game    
+  end
+  
+  def play(x, y)
+    if winner?
+      puts "#{@current_player} wins!"
+    else
+      array = []
+      array << x << y
+      @plays[@current_player] << array
+      @current_player = (@current_player == 'X') ? 'O' : 'X'      
+    end
+  end
+  
+  def winner?
+    if @plays['X'] == @horizontal_winner || @plays['O'] == @horizontal_winner
+      true
+    elsif @plays['X'] == @vertical_winner || @plays['O'] == @vertical_winner
+      true
+    elsif @plays['X'] == @diagonal_winner || @plays['O'] == @diagonal_winner
+      true
+    else
+      false
+    end
+  end
+  
+end
